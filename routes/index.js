7486b6d51124e70944bc27558bcce637a024e4c1
princehash90 <hashim.muridi@go.minneapolis.edu>
2024-11-17 18:39:42 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express'); // importing the express library
+const router = express.Router();// we make router here
+const randomWyrQuestion=require('../model/randomWyrQuestion');
+//.. means go up by index.js
+//randomWyrQuestion is going to have whatever that will be exported from the function on wyr_question.js
+
+
+//router that handle homepage
+router.get('/', (req, res) => {
+    res.send ('placeholder for home page')
+})
+//we add possible router it can respond to
+router.get ('/wyr', function ( req, res, next){
+    // const wyr = {
+    //     'question':'Live in a circle house or a triangle house',
+    //     'answer1':' Circle house',
+    //     'answer2':' Triangle house'
+    // }
+    const wyr = randomWyrQuestion()// now calling on the function in the model directory
+    res.json(wyr)
+})
+
+module.exports = router; //this router will become indexRouter in the server.js
---
+++ server.js
@@ -0,0 +1,12 @@
+const express =require('express')
+
+const indexRouter=require('./routes/index')
+
+const app = express()
+
+app.use('/', indexRouter)
+
+// the port we want to use
+const server = app.listen(process.env.PORT || 3000, function (){
+    console.log("Server started on port " + server.address().port);
+});
